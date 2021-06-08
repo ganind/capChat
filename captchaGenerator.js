743ebf50a76eb6e3518f5595eefe56e0e3e27e8a
ganind <deborahganin@gmail.com>
2021-06-08 16:30:05 +0200

    fix images display + start mouse tracking

--- captchaGenerator.js
+++ captchaGenerator.js
@@ -18,43 +18,43 @@ function loadNeutre() {
             chemin = data[0].chemin;
             base_image.src = "http://localhost:3000/img/" + chemin;
             base_image.onload = function () {
-                canvas.drawImage(base_image, 0, 0, 100, 100);
+                ctx.drawImage(base_image, 0, 0, 85, 100);
             }
             let base_image1 = new Image();
             chemin = data[1].chemin;
             base_image1.src = "http://localhost:3000/img/" + chemin;
             base_image1.onload = function () {
-                canvas.drawImage(base_image1, 85, 0, 100, 100);
+                ctx.drawImage(base_image1, 85, 0, 85, 100);
             }
             let base_image2 = new Image();
             chemin = data[2].chemin;
             base_image2.src = "http://localhost:3000/img/" + chemin;
             base_image2.onload = function () {
-                canvas.drawImage(base_image2, 170, 0, 100, 100);
+                ctx.drawImage(base_image2, 170, 0, 85, 100);
             }
             let base_image3 = new Image();
             chemin = data[3].chemin;
             base_image3.src = "http://localhost:3000/img/" + chemin;
             base_image3.onload = function () {
-                canvas.drawImage(base_image3, 255, 0, 100, 100);
+                ctx.drawImage(base_image3, 255, 0, 85, 100);
             }
             let base_image4 = new Image();
             chemin = data[4].chemin;
             base_image4.src = "http://localhost:3000/img/" + chemin;
             base_image4.onload = function () {
-                canvas.drawImage(base_image4, 0, 100, 100, 100);
+                ctx.drawImage(base_image4, 0, 100, 85, 100);
             }
             let base_image5 = new Image();
             chemin = data[5].chemin;
             base_image5.src = "http://localhost:3000/img/" + chemin;
             base_image5.onload = function () {
-                canvas.drawImage(base_image5, 85, 100, 100, 100);
+                ctx.drawImage(base_image5, 85, 100, 85, 100);
             }
             let base_image6 = new Image();
             chemin = data[6].chemin;
             base_image6.src = "http://localhost:3000/img/" + chemin;
             base_image6.onload = function () {
-                canvas.drawImage(base_image6, 170, 100, 100, 100);
+                ctx.drawImage(base_image6, 170, 100, 85, 100);
             }
         }
     };
@@ -75,7 +75,7 @@ function loadSinguliere() {
             chemin = data[0].chemin;
             base_image.src = "http://localhost:3000/img/" + chemin;
             base_image.onload = function () {
-                canvas.drawImage(base_image, 255, 100, 100, 100);
+                ctx.drawImage(base_image, 255, 100, 85, 100);
             }
             astuce = data[0].indice;
             document.getElementById("astuce").textContent = astuce;
@@ -87,7 +87,8 @@ function loadSinguliere() {
 }
 
 function setCanvas() {
-     canvas = document.getElementById("captchaCanvas").getContext("2d");
+     canvas = document.getElementById("captchaCanvas");
+     ctx = canvas.getContext("2d");
      canvas.addEventListener('click', handleClick);
 }
 
@@ -101,18 +102,38 @@ function chrono() {
     setTimeout('chrono()', 1000); //en milliseconds
 }
 
-// function getMousePos(c, evt) {
-//     let rect = c.getBoundingClientRect();
-//     return{
-//         x: evt.clientX - rect.left,
-//         y: evt.clientY - rect.top
-//     };
-// }
-//
-// function handleClick(e) {
-//     let pos = getMousePos(canvas, e);
-//     posx = pos.x;
-//     posy = pos.y;
-//
-//     alert(posx + " " + posy);
-// }
+function getMousePos(c, evt) {
+    let rect = c.getBoundingClientRect();
+    return{
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function handleClick(e) {
+    let pos = getMousePos(canvas, e);
+    posx = pos.x;
+    posy = pos.y;
+    alert(posx + " " + posy);
+}
+
+function imageChosen(x,y) {
+    if (x < 85 && y < 100) {
+        return 1;
+    }
+    else if (x <= 170 && y < 100) {
+        return 2;
+    }
+    else if (x <= 255 && y < 100) {
+        return 3;
+    }
+    else if (x <= 85 && y >= 100) {
+        return 4;
+    }
+    else if (x <= 170 && y >= 100) {
+        return 5;
+    }
+    else if (x <= 255 && y >= 100) {
+        return 6;
+    }
+}
